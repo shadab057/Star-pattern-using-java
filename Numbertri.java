b0278d593ac89eaf474873c22c2888667af994df
shadab057 <mohdshadab4629@gmail.com>
2025-01-21 00:14:40 +0530

    Number-increasing pyramid pattern

---
+++ Numbertri.java
@@ -0,0 +1,25 @@
+public class Numbertri {
+    public static void printPattern(int n)
+    {
+        int i, j;
+        
+        for (i = 1; i <= n; i++) {
+           
+            for (j = 1; j <= i; j++) {
+            
+               
+                System.out.print(j + " ");
+            }
+
+            // print new line for each row
+            System.out.println();
+        }
+    }
+
+ 
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
